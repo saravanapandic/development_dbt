fdf0b68117eacedc9f2a196b22679159992a77bb
swetha P <sweetswetha5321@gmail.com>
2023-11-29 13:00:07 +0000

    changes in student

--- models/School/student.sql
+++ models/School/student.sql
@@ -2,11 +2,11 @@ with source_data as (
 
     select 1 as id,'swetha' as name, 21 as age
     union all
-    select 1 as id, 'saravana' as name, 22 as age
+    select 2 as id, 'saravana' as name, 22 as age
     union all
-    select 2 as id, 'shajahan' as name, 22 as age
+    select 3 as id, 'shajahan' as name, 22 as age
     union all
-    select null as id, 'sashanth' as name, 22 as age
+    select 4 as id, 'sashanth' as name, 22 as age
 
 )
 
