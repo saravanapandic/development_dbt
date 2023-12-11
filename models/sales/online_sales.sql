2382729715b1e87c4383af497faada92671cdfe7
swetha5321 <96857573+swetha5321@users.noreply.github.com>
2023-12-11 16:25:14 +0530

    Update online_sales.sql

--- models/sales/online_sales.sql
+++ models/sales/online_sales.sql
@@ -1,1 +1,7 @@
+[4:24 PM] Saravanapandi C
+{{
+    config(
+        materialized='incremental'
+    )
+}}
 SELECT $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14 FROM @sales_stage WHERE $4 = 'Online'
