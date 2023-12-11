deb9a80a49e581aa79f782a3eabd6f447e8929da
swetha5321 <96857573+swetha5321@users.noreply.github.com>
2023-12-11 17:31:25 +0530

    Update profit_by_product.sql

--- models/sales/profit_by_product.sql
+++ models/sales/profit_by_product.sql
@@ -3,4 +3,4 @@
         materialized='incremental'
     )
 }}
-SELECT $3 as Products,sum($14) as Total_profit FROM @sales_stage group by $3
+SELECT $3 as Products,sum($9) as units_sold,$11 as Cost_Price, $10 as selling_price,sum($12) as Total_revenue, sum($14) as Total_profit FROM @my_ext_stage group by $3,$11,$10
