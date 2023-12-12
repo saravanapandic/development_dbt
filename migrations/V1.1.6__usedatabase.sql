c979d044a1991e2455b87b522b1ea37da58bf275
saravana pandi <130064336+saravanapandic@users.noreply.github.com>
2023-12-12 14:22:44 +0530

    Update V1.1.6__usedatabase.sql

--- migrations/V1.1.6__usedatabase.sql
+++ migrations/V1.1.6__usedatabase.sql
@@ -19,3 +19,8 @@ CREATE OR REPLACE STAGE sales_stage
   URL='azure://swethastorageaccount.blob.core.windows.net/csv'
   STORAGE_INTEGRATION = azure_integration
   FILE_FORMAT= my_csv_format;
+
+CREATE OR REPLACE FILE FORMAT my_csv_format
+  TYPE = CSV
+  SKIP_HEADER = 1;
+
