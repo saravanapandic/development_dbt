7b333539a53449a20ffa4dccb990e76808141446
swetha5321 <96857573+swetha5321@users.noreply.github.com>
2023-12-19 15:53:18 +0530

    Update profit_by_product.sql

--- models/sales/profit_by_product.sql
+++ models/sales/profit_by_product.sql
@@ -3,4 +3,11 @@
         materialized='incremental'
     )
 }}
-SELECT $3 as Products,sum($9) as units_sold,$11 as Cost_Price, $10 as selling_price,sum($12) as Total_revenue, sum($14) as Total_profit FROM @sales_stage group by $3,$11,$10
+SELECT 
+    $3::varchar as Products,
+    sum($9)::number as units_sold,
+    $11::float as Cost_Price,
+    $10::float as selling_price,
+    sum($12)::float as Total_revenue, 
+    sum($14)::float as Total_profit 
+FROM @sales_stage group by $3,$11,$10
