1ff912b51156f616dce7fcfffc6774c96453ecf0
saravana pandi <130064336+saravanapandic@users.noreply.github.com>
2023-12-20 15:29:42 +0530

    Update Yearwise_profit.sql

--- models/sales/Yearwise_profit.sql
+++ models/sales/Yearwise_profit.sql
@@ -3,8 +3,4 @@
         materialized='incremental'
     )
 }}
-
-SELECT 
-    year(cast($8 as date))::number as years,
-    sum($14) as total_profit::float
-FROM @sales_stage group by years order by years
+SELECT year(cast($8 as date)) as years,sum($14) as total_profit FROM @sales_stage group by years order by years
