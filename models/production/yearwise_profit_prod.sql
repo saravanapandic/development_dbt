70f5b655827d8f1005fd70b58e7d6aa56db2dda5
saravana pandi <130064336+saravanapandic@users.noreply.github.com>
2023-12-20 15:33:31 +0530

    Update yearwise_profit_prod.sql

--- models/production/yearwise_profit_prod.sql
+++ models/production/yearwise_profit_prod.sql
@@ -4,7 +4,4 @@
     )
 }}
 
-SELECT 
-    year(cast($8 as date))::number as years,
-    sum($14) as total_profit::float
-FROM @sales_stage group by years order by years
+SELECT year(cast($8 as date)) as years,sum($14) as total_profit FROM @sales_stage group by years order by years
