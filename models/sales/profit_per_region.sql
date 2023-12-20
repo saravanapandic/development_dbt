87254cb1a87fdc4af030d62e2adccdb7f57fa5b6
swetha5321 <96857573+swetha5321@users.noreply.github.com>
2023-12-20 14:21:17 +0530

    Update profit_per_region.sql

--- models/sales/profit_per_region.sql
+++ models/sales/profit_per_region.sql
@@ -3,4 +3,7 @@
         materialized='incremental'
     )
 }}
-SELECT $1 as region, sum($14) as total_profit FROM @sales_stage group by $1
+SELECT 
+    $1::varchar as region, 
+    sum($14)::float as total_profit 
+FROM @sales_stage group by $1
