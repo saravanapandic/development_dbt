ae87fb435382ae08f06fc4b1ff581a3d6b7f61f4
saravana pandi <130064336+saravanapandic@users.noreply.github.com>
2023-12-22 12:29:41 +0530

    Update Yearwise_profit.sql

--- models/sales/Yearwise_profit.sql
+++ models/sales/Yearwise_profit.sql
@@ -1,6 +1,7 @@
 {{
     config(
-        materialized='incremental'
+        materialized='incremental',database="DBT_GITHUB"
     )
 }}
+   
 SELECT year(cast($8 as date)) as years,sum($14) as total_profit FROM @sales_stage group by years order by years
