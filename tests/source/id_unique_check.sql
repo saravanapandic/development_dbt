aeabe6234a81f185c982873ccd9907f1e72d5dc6
saravana pandi <130064336+saravanapandic@users.noreply.github.com>
2023-12-22 12:05:19 +0530

    Update id_unique_check.sql

--- tests/source/id_unique_check.sql
+++ tests/source/id_unique_check.sql
@@ -1,2 +1,6 @@
-USE DATABASE DBT_GITHUB;
+{{
+    config(
+       database="DBT_GITHUB"
+    )
+}}
 SELECT $7, COUNT($7) as counts FROM @DBT_GITHUB.SALES_DETAILS.SALES_STAGE group by $7 HAVING counts > 1;
