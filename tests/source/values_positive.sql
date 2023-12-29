1ea90b5c3df79ddb638e9a663554a7e6744193df
saravana pandi <saravanapandic.19mech@kongu.edu>
2023-12-29 06:57:31 +0000

    source database is change

--- tests/source/values_positive.sql
+++ tests/source/values_positive.sql
@@ -1,1 +1,6 @@
+{{
+    config(
+       database="STAGE_DB",schema="sales_details"
+    )
+}}
 SELECT $1,$9,$10,$11,$12,$13,$14 FROM @STAGE_DB.SALES_DETAILS.SALES_STAGE WHERE $9 < 0 OR $10 < 0 OR $11 < 0 OR $12 < 0 OR $13 < 0 OR $14 < 0
