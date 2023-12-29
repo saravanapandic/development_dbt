bb6c51616f53094f5dfdc7f06012799f06643be3
saravana pandi <saravanapandic.19mech@kongu.edu>
2023-12-29 06:57:58 +0000

    h

--- tests/source/id_unique_check.sql
+++ tests/source/id_unique_check.sql
@@ -1,6 +1,6 @@
 {{
     config(
-       database="DBT_GITHUB",schema="sales_details"
+       database="STAGE_DB",schema="sales_details"
     )
 }}
 SELECT $7, COUNT($7) as counts FROM @STAGE_DB.SALES_DETAILS.SALES_STAGE group by $7 HAVING counts > 1
