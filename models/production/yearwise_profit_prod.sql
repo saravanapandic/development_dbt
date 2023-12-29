23c019d8705f0cf6b6e0603b1401c9c7699e7bed
saravana pandi <saravanapandic.19mech@kongu.edu>
2023-12-29 05:12:55 +0000

    target base is change

--- models/production/yearwise_profit_prod.sql
+++ models/production/yearwise_profit_prod.sql
@@ -1,6 +1,6 @@
 {{
     config(
-        materialized='incremental',database="production_dbt",schema='SALES_DETAILS',incremental_strategy = 'merge'
+        materialized='incremental',database="target_DB",schema='SALES_DETAILS',incremental_strategy = 'merge'
     )
 }}
 
