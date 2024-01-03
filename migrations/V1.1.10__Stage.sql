de1831d00ab8a3f60703186856b36650eb9f9836
saravana pandi <130064336+saravanapandic@users.noreply.github.com>
2024-01-03 15:40:24 +0530

    Update V1.1.10__Stage.sql

--- migrations/V1.1.10__Stage.sql
+++ migrations/V1.1.10__Stage.sql
@@ -9,4 +9,6 @@ CREATE OR REPLACE STAGE STAGE_DB.SALES_DETAILS.sales_stage
   URL='azure://azurestoragedbt.blob.core.windows.net/source/sales_record'
   STORAGE_INTEGRATION = azure_integration
   FILE_FORMAT= my_csv_format;
-
+CREATE OR REPLACE FILE FORMAT Stage_DB.Public.my_csv_format
+  TYPE = CSV
+  SKIP_HEADER = 1;
