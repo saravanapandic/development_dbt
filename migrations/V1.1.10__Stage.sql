99b8f716743e2868845a63c688f0819e1e45e965
saravana pandi <130064336+saravanapandic@users.noreply.github.com>
2024-01-03 15:14:13 +0530

    Update V1.1.10__Stage.sql

--- migrations/V1.1.10__Stage.sql
+++ migrations/V1.1.10__Stage.sql
@@ -1,3 +1,6 @@
+use database Stage_DB;
+use schema sales_details;
+
 CREATE OR REPLACE STAGE STAGE_DB.SALES_DETAILS.sales_stage
   URL='azure://azurestoragedbt.blob.core.windows.net/source/sales_record'
   STORAGE_INTEGRATION = azure_integration
